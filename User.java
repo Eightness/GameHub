efd78f736215d09565b00f7e10ff1b5a06efffa9
Marrahy <sergimarenas@gmail.com>
2023-03-23 19:27:18 +0100

    User, UserManager Done

--- User.java
+++ User.java
@@ -5,19 +5,89 @@
 public class User {
 
     //Enum
-
+    public enum UserType {
+        ADMIN, MOD, USER
+    }
     //Class attributes
-
+    String name, username, mail, password;
+    UserType type;
     //----------------------------------------------------------------------
 
-    //Constructors
-
+    //Void constructor
+    public User() {
+        
+    }
+    //Constructor
+    public User(String name, String username, String password, UserType type) {
+        setName(username);
+        setUsername(username);
+        setPassword(password);
+        setUsername(username);
+    }
     //----------------------------------------------------------------------
 
-    //Setters and Getters
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setType(UserType type) {
+        this.type = type;
+    }
 
+    //Getters
+    public String getName() {
+        return name;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public UserType getType() {
+        return type;
+    }
     //----------------------------------------------------------------------
 
     //Functions
+    public void showUser() {
+        System.out.println();
+        System.out.println("Nombre: " + getName());
+        System.out.println("Apodo: " + getUsername());
+        System.out.println("Email: " + getMail());
+        System.out.println("Tipo de usuario: " + getType());
+        System.out.println();
+    }
 
+    public boolean isName(String name) {
+        return getName().equalsIgnoreCase(name);
+    }
+
+    public boolean isUserName(String username) {
+        return getUsername().equalsIgnoreCase(username);
+    }
+
+    public boolean isMail(String mail) {
+        return getMail().contains(mail);
+    }
+
+    public boolean correctPassword(String password) {
+        return getPassword().equals(password);
+    }
+
+    public boolean isUserType(UserType type) {
+        return getType() == type;
+    }
 }
--- UserManager.java
+++ UserManager.java
@@ -5,17 +5,106 @@
 public class UserManager {
 
     //Class attributes
-
+    int maxUsers = 10;
+    int numUsers = 0;
+    User[] users = new User[maxUsers];
     //----------------------------------------------------------------------
 
-    //Constructors
+    //Void constructor
+    public UserManager() {
 
+    }
+    //Constructor
+    public UserManager(int maxUsers) {
+        setMaxUsers(maxUsers);   
+    }
     //----------------------------------------------------------------------
 
-    //Setters and Getters
+    //Setters
+    public void setMaxUsers(int maxUsers) {
+        this.maxUsers = maxUsers;
+    }
+    public void setNumUsers(int numUsers) {
+        this.numUsers = numUsers;
+    }
+    public void setUsers(User[] users) {
+        this.users = users;
+    }
 
+    //Getters
+    public int getMaxUsers() {
+        return maxUsers;
+    }
+    public int getNumUsers() {
+        return numUsers;
+    }
+    public User[] getUsers() {
+        return users;
+    }
     //----------------------------------------------------------------------
 
     //Functions
+    public boolean isEmpty() {
+        return users[0] == null;
+    }
+
+    public boolean isFull() {
+        return numUsers == maxUsers;
+    }
     
+    public void addUser(User user) {
+        users[numUsers] = user;
+        numUsers++;
+    }
+
+    public void removeUser(int pos) {
+        users[pos] = null;
+        for (int i = pos; i < numUsers - 1; i++) {
+            users[i] = users[i + 1];
+        }
+        numUsers--;
+    }
+
+    public void showUsers() {
+        for (int i = 0; i < numUsers; i++) {
+            System.out.println("Usuario " + (i + 1) + ":");
+            users[i].showUser();
+        }
+    }
+
+    public void filterByName(String name) {
+        for (int i = 0; i < numUsers; i++) {
+            if (users[i].isName(name)) {
+                System.out.println("Usuario " + (i + 1) + ":");
+                users[i].showUser();
+            }
+        }
+    }
+
+    public void filterByUserName(String username) {
+        for (int i = 0; i < numUsers; i++) {
+            if (users[i].isUserName(username)) {
+                System.out.println("Usuario " + (i + 1) + ":");
+                users[i].showUser();
+            }
+        }
+    }
+
+    public void filterByMail(String mail) {
+        for (int i = 0; i < numUsers; i++) {
+            if (users[i].isMail(mail)) {
+                System.out.println("Usuario " + (i + 1) + ":");
+                users[i].showUser();
+            }
+        }
+    }
+
+    public void filterByUserType(User.UserType type) {
+        for (int i = 0; i < numUsers; i++) {
+            if (users[i].isUserType(type)) {
+                System.out.println("Usuario " + (i + 1) + ":");
+                users[i].showUser();
+            }
+        }
+    }
 }

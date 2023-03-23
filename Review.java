b4c72d75f013d5f619a2297744228c17d5264fbd
Marrahy <sergimarenas@gmail.com>
2023-03-23 19:25:15 +0100

    User, UserManager_Done Review wip

--- Review.java
+++ Review.java
@@ -1,3 +1,4 @@
+
 //Albert Lozano y Sergi Marrahy 
 //1 DAM - Programación
 
@@ -5,17 +6,88 @@
 public class Review {
 
     //Class attributes
-
+    String title, body, date;
+    int rating;
+    double playedHours;
+    Videogame videogame;
+    User user;
     //----------------------------------------------------------------------
 
-    //Constructors
+    //Void constructor
+    public Review() {
 
+    }
+
+    //Constructor
+    public Review(String title, String body, int rating, String date, Double playedHours, Videogame videogame, User user) {
+        setTitle(title);
+        setBody(body);
+        setRating(rating);
+        setDate(date);
+        setPlayedHours(playedHours);
+        setVideogame(videogame);
+        setUser(user);
+    }
+    
     //----------------------------------------------------------------------
 
-    //Setters and Getters
+    //Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public void setPlayedHours(double playedHours) {
+        this.playedHours = playedHours;
+    }
+    public void setVideogame(Videogame videogame) {
+        this.videogame = videogame;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
 
+    //Getters
+    public String getTitle() {
+        return title;
+    }
+    public String getBody() {
+        return body;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public String getDate() {
+        return date;
+    }
+    public double getPlayedHours() {
+        return playedHours;
+    }
+    public Videogame getVideogame() {
+        return videogame;
+    }
+    public User getUser() {
+        return user;
+    }
     //----------------------------------------------------------------------
 
     //Functions
-    
+    public void showReview() {
+        System.out.println();
+        System.out.println("Título: " + getTitle());
+        System.out.println("Cuerpo: " + getBody());
+        System.out.println("Valoración: " + getRating() + "⭐");
+        System.out.println("Usuario: " + getUser());
+        System.out.println("Videojuego: " + getVideogame());
+        System.out.println("Horas jugadas: " + getPlayedHours());
+        System.out.println("Fecha de publicación: " + getDate());
+        System.out.println();
+    }
 }

0f8509961addbbac89b90441da11fca9297d67a0
Albert <albertlb08@gmail.com>
2023-03-23 16:16:00 +0100

    Final preparations, plus Main class - Albert

--- Main.java
+++ Main.java
@@ -3,5 +3,13 @@
 
 //Class done by both
 public class Main {
+
+    //MAIN
+    public static void main(String[] args) {
+
+        GameHub gameHub = new GameHub();
+        gameHub.start();
+        
+    }
     
 }
